5146bbce3ba3909b0f70b6453fdd0777f24e7429
Jim Raden <james.raden@gmail.com>
2021-04-15 22:59:57 -0400

    Enable decodeString

--- main.go
+++ main.go
@@ -26,17 +26,44 @@ func encodeBytes(r *bytes.Buffer, w io.Writer) {
 	w.Write([]byte(encoded))
 }
 
+func decodeString(r io.Reader, w io.Writer) {
+	sourceBytes := make([]byte, 1024)
+	n, err := r.Read(sourceBytes)
+	if err != nil {
+		panic(err)
+	}
+
+	sourceBytes = sourceBytes[0:n]
+	w.Write(base58.Decode(string(sourceBytes)))
+}
+
+// getSuppliedFlags gets all flags that were actually entered on the command line
+func getSuppliedFlags() map[string]*flag.Flag {
+	visitedFlags := map[string]*flag.Flag{}
+	flag.Visit(func(flag *flag.Flag) {
+		visitedFlags[flag.Name] = flag
+	})
+	return visitedFlags
+}
+
 // TODO: add parameters to allow specifying length, how many, etc.
 func main() {
 	encFlag := flag.Bool("e", true, "Encode")
 	decFlag := flag.Bool("d", false, "Decode")
-	numberFlag := flag.Int("n", 0, "Number of random bytes to generate.  Implies -e."+
-		"If absent, ")
+	numberFlag := flag.Int("n", 0, "Number of random bytes to generate.  Implies -e.  "+
+		"If absent, read from stdin")
 	flag.Parse()
 
-	if *decFlag {
+	suppliedFlags := getSuppliedFlags()
+	if *encFlag && *decFlag && suppliedFlags["d"] != nil && suppliedFlags["e"] != nil {
+		panic("Cannot specify both -d and -e")
+	}
+
+	if *decFlag && suppliedFlags["e"] == nil {
+		// They haven't explicitly specified -e, so set the encFlag to false
 		*encFlag = false
 	}
+
 	if *numberFlag > 1024 {
 		panic("-n is limited to 1024 bytes")
 	}
@@ -51,6 +78,7 @@ func main() {
 		b = b[0:n]
 		source = bytes.NewBuffer(b)
 	} else {
+		// TODO: can't decode random bytes
 		randBytes := make([]byte, *numberFlag)
 		_, err := rand.Read(randBytes)
 		if err != nil {
@@ -58,13 +86,14 @@ func main() {
 		}
 		source = bytes.NewBuffer(randBytes)
 	}
-	*encFlag = true
 
-	if *decFlag {
-		panic("Cannot specify -d and -e.  (-e is implied by -n)")
+	switch {
+	case *encFlag:
+		encodeBytes(source, os.Stdout)
+
+	case *decFlag:
+		decodeString(source, os.Stdout)
 	}
-
-	encodeBytes(source, os.Stdout)
 
 	if isTerminal() {
 		// Pretty-print by adding a newline to the output
